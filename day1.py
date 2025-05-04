b7793d7d7aae328c00ca9791bc4255b94d636c1a
AmtTawsik <amttawsik99@gmail.com>
2025-05-04 23:12:02 +0600

    first commit

---
+++ day1.py
@@ -0,0 +1,9 @@
+# Exercise 1:
+Length = 10
+Width= 15
+area = Length*Width
+print("Area of a rectangle is:",area)
+
+# Exercise 2:
+favorite_programming_languages = ["Python", "JavaScript", "Go"]
+print("My favorite programming languages are:",favorite_programming_languages)
